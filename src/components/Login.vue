e4a6902dea2bf0c8232477f773975e26971d3748
sol_tsui <cysuper0921@gmail.com>
2020-01-08 15:02:31 +0800

    修改git-email

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -38,7 +38,7 @@
           <el-input
             prefix-icon="iconfont icon-mima"
             v-model="loginForm.pwd"
-            type="text"
+            type="text" 
             placeholder="输入密码"
           ></el-input>
         </el-form-item>
